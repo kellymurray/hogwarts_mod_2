cf0ce991cb2b87708ea94de66f3e15fa38ac4881
Kelly Murray <kmmurray17@gmail.com>
2021-09-14 10:44:27 -0500

    complete user story 3

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student <ApplicationRecord
   has_many :professor_students
   has_many :professors, through: :professor_students
   validates :name, :age, :house, presence: true
+
+
+  def professor_count
+    professors.count
+  end
+
 end
